8dc8c4d756cd0e16211bd42454e0b47037d89f63
Christian Andrade <andrade.dias.christian@gmail.com>
2022-10-28 01:08:14 -0300

    create post patch and add req.body

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const livros = [
     {id: 1, "titulo": "Senhor dos Aneis"},
@@ -16,5 +17,11 @@ app.get("/livros", (req, res) => {
     res.status(200);
     res.json(livros);
 });
+
+app.post("/livros", (req, res) => {
+    livros.push((req.body));
+    res.status(201);
+    res.send("Livros cadastrado.");
+})
 
 export default app;
