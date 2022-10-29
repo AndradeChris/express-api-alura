1589190eb8207b528cd8e5749663bbb8dd07e5bf
Christian Andrade <andrade.dias.christian@gmail.com>
2022-10-28 21:36:50 -0300

    create put and get by id

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ const app = express();
 app.use(express.json());
 
 const livros = [
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobbit"}
+    {id: 1, titulo: "Senhor dos Aneis"},
+    {id: 2, titulo: "O Hobbit"}
 ];
+
+const buscaId = (id) => {
+    const index = livros.findIndex(livro => livro.id == id)
+    return index
+}
 
 app.get("/",(req, res) => {
     res.status(200);
@@ -18,10 +23,23 @@ app.get("/livros", (req, res) => {
     res.json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaId(req.params.id);
+    res.status(200);
+    res.json(livros[index])
+})
+
 app.post("/livros", (req, res) => {
     livros.push((req.body));
     res.status(201);
     res.send("Livros cadastrado.");
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaId(req.params.id);
+    livros[index].titulo = req.body;
+    res.status(200);
+    res.json(livros);
 })
 
 export default app;
